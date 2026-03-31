bc9032401330446632c220c1220d2a5ae95db145
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2026-03-31 08:41:10 -0500

    feat: favicon assets update

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,11 +29,11 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon.ico" },
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: "/apple-touch-icon.png",
+    apple: "/icons/apple-touch-icon.png",
   },
   manifest: "/site.webmanifest",
 
